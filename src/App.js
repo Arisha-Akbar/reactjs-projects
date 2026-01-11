7d8d73fe83942b2dcf699bb8b2d9653dc2c90930
Arisha-Akbar <arishaakbar713@gmail.com>
2026-01-11 17:24:51 +0500

    Added ImageSlider component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import './App.css';
-import Accordian from './components/accordian';
-import RandomColor from './components/random-color';
-import StarRating from './components/star-rating';
+import "./App.css";
+import Accordian from "./components/accordian";
+import ImageSlider from "./components/image-slider";
+import RandomColor from "./components/random-color";
+import StarRating from "./components/star-rating";
 
 function App() {
   return (
     <div className="App">
-     <Accordian />
+      {/* <Accordian />
      <RandomColor/>
-     <StarRating noOfStars={10}/>
+     <StarRating noOfStars={10}/> */}
+      <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        page={"1"}
+        limit={"10"}
+      />
     </div>
   );
 }
